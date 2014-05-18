ff850e4dab98a7dcb5363a46be9f0c26c0145ded
SoXeon <dpsopower@gmail.com>
2014-05-18 19:51:20 +0800

    Initial Commit

---
+++ 要来一发吗/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  要来一发吗
+//
+//  Created by DP on 14-5-18.
+//  Copyright (c) 2014年 M2A1102. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DPAppDelegate class]));
+    }
+}
---
+++ 要来一发吗Tests/_____Tests.m
@@ -0,0 +1,34 @@
+//
+//  _____Tests.m
+//  要来一发吗Tests
+//
+//  Created by DP on 14-5-18.
+//  Copyright (c) 2014年 M2A1102. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _____Tests : XCTestCase
+
+@end
+
+@implementation _____Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
