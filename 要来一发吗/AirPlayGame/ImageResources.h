ab1e54dfab7a0cd394c9a47e2acdc752f3863134
SoXeon <dpsopower@gmail.com>
2014-05-20 18:40:28 +0800

    增加了飞机爆炸时候到效果

--- 要来一发吗/AirPlayGame/ImageResources.h
+++ 要来一发吗/AirPlayGame/ImageResources.h
@@ -27,6 +27,23 @@
 //加强子弹
 @property (strong,nonatomic) UIImage *bulletEnhancedImage;
 
+#pragma mark 敌机图片
+//小飞机飞行图
+@property (strong,nonatomic) UIImage *enemySmallImage;
+//小飞机爆炸的数组
+@property (strong,nonatomic) NSArray *enemySmallBlowupImages;
+//中飞机飞行的图像
+@property (strong,nonatomic) UIImage *enemyMiddleImage;
+//中飞机爆炸的数组
+@property (strong,nonatomic) NSArray *enemyMiddleBlowupImages;
+//中飞机被打的图片
+@property (strong,nonatomic) UIImage *enemyMiddleHitImages;
+//大飞机飞行的图片
+@property (strong,nonatomic) NSArray *enemyBigImage;
+//大飞机爆炸的数组
+@property (strong,nonatomic) NSArray *enemyBigBlowupImages;
+//大飞机被打打图片
+@property (strong,nonatomic) UIImage *enemyBigHitImages;
 
 
 @end
